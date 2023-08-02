4a43f1bafbb4903c1b0e9fb697695635695d1ad6
Waseem Syed <syed.waseem87@gmail.com>
2023-08-02 12:16:51 +0530

    Added logic to read a file word by word and store the count in a map

--- src/main/java/org/example/WordCount.java
+++ src/main/java/org/example/WordCount.java
@@ -1,7 +1,33 @@
 package org.example;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
 public class WordCount {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        Map<String, Integer> wordCounts = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(Files.newInputStream(Paths.get("gpl-3.0.txt")), StandardCharsets.UTF_8))) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                StringTokenizer tokenizer = new StringTokenizer(line, " ");
+                while (tokenizer.hasMoreTokens()) {
+                    String currentWord = tokenizer.nextToken().toLowerCase();
+                    if (wordCounts.containsKey(currentWord)) {
+                        wordCounts.put(currentWord, wordCounts.get(currentWord) + 1);
+                    } else {
+                        wordCounts.put(currentWord, 1);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(wordCounts);
     }
 }
