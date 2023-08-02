d96af1f9fe460b80d6fbcb8bff003996f9609395
Waseem Syed <syed.waseem87@gmail.com>
2023-08-02 14:16:53 +0530

    Refactored code and added logic to ignore special characters

--- src/main/java/org/example/WordCount.java
+++ src/main/java/org/example/WordCount.java
@@ -1,6 +1,10 @@
 package org.example;
 
-import java.io.*;
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -10,24 +14,39 @@ import java.util.StringTokenizer;
 
 public class WordCount {
     public static void main(String[] args) {
-        Map<String, Integer> wordCounts = new HashMap<>();
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(Files.newInputStream(Paths.get("gpl-3.0.txt")), StandardCharsets.UTF_8))) {
-            String line;
+        WordCount wordCount = new WordCount();
+        Map<String, Integer> wordCounts = wordCount.getWordCountMap(args[0]);
+        wordCount.printWordCountJson(wordCounts);
+    }
 
+    void printWordCountJson(Map<String, Integer> wordCounts) {
+        System.out.println(new Gson().toJson(wordCounts));
+    }
+
+    Map<String, Integer> getWordCountMap(String filePath) {
+        Map<String, Integer> wordCounts = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(Files.newInputStream(Paths.get(filePath)), StandardCharsets.UTF_8))) {
+            String line;
             while ((line = br.readLine()) != null) {
                 StringTokenizer tokenizer = new StringTokenizer(line, " ");
                 while (tokenizer.hasMoreTokens()) {
-                    String currentWord = tokenizer.nextToken().toLowerCase();
-                    if (wordCounts.containsKey(currentWord)) {
-                        wordCounts.put(currentWord, wordCounts.get(currentWord) + 1);
-                    } else {
-                        wordCounts.put(currentWord, 1);
-                    }
+                    getWordCounts(tokenizer, wordCounts);
                 }
             }
         } catch (IOException e) {
+            System.out.println("IOException occurred");
             throw new RuntimeException(e);
         }
-        System.out.println(wordCounts);
+        return wordCounts;
+    }
+
+    void getWordCounts(StringTokenizer tokenizer, Map<String, Integer> wordCounts) {
+        String currentWord = tokenizer.nextToken().toLowerCase().replaceAll("[^A-Za-z]", "");
+
+        if (wordCounts.containsKey(currentWord)) {
+            wordCounts.put(currentWord, wordCounts.get(currentWord) + 1);
+        } else {
+            wordCounts.put(currentWord, 1);
+        }
     }
 }
